6bde87ce6e19387fdc76e1bc27165e93f1c14afd
malmagen <fredrikpersson84@gmail.com>
2016-04-27 10:03:37 +0200

    add priority function

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -20,6 +20,7 @@ var app = app || {};
 			'click .toggle': 'toggleCompleted',
 			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -51,6 +52,8 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
+
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -66,6 +69,12 @@ var app = app || {};
 				app.TodoFilter === 'completed';
 		},
 
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function() {
+			this.model.togglePriority();
+			
+			
+		},
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
@@ -107,7 +116,7 @@ var app = app || {};
 
 			this.$el.removeClass('editing');
 		},
-
+		
 		// If you hit `enter`, we're through editing the item.
 		updateOnEnter: function (e) {
 			if (e.which === ENTER_KEY) {
